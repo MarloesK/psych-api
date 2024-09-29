534258c38b26f6f2480bd73c76ad6fec8ff9a3af
Marloes <marloes.madelon@gmail.com>
2024-09-29 21:23:32 +0200

    upgrade to python 3.12, restructure repo

--- src/app.py
+++ src/app.py
@@ -3,13 +3,14 @@ from typing import List, Optional
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from mangum import Mangum
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 
-from helpers import load_json
-from models import Pineapple
+from src.helpers import load_json
+from src.models import Pineapple
 
 app = FastAPI()
 limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
@@ -20,7 +21,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello Psych Fan!🍍"}
 
 
@@ -79,3 +80,6 @@ def read_nicknames(limit: int = 5):
 def read_trivia(limit: int = 5):
     data = load_json('data/trivia.json')
     return JSONResponse(content=data[:limit])
+
+
+handler = Mangum(app)
